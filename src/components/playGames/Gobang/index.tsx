11117b8d7031b2e652a1dbd174f072c23393f7bc
liuyuan <15713621441@163.com>
2023-11-14 17:00:23 +0800

    feat: 增加电脑落子

--- src/components/playGames/Gobang/index.tsx
+++ src/components/playGames/Gobang/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './index.scss';
 import { Button, message } from 'antd';
 import { useNavigate } from 'react-router-dom';
@@ -30,7 +30,8 @@ const Gobang = () => {
 
     const [pieces, setPieces] = useState<Gobang[][]>(generateData())    //  棋子数据
     const [rotationColor, setRotationColor] = useState<boolean>(true) //  轮换棋子颜色 true:'black' | false: 'white'
-    const [gameState, setGameState] = useState<string>('NotStart') // 游戏状态  NotStart/未开始 Start/开始 Suspend/暂停 Over/结束 Win/胜利
+    const [gameState, setGameState] = useState<string>('NotStart') // 游戏状态（异步，存在延迟）  NotStart/未开始 Start/开始 Suspend/暂停 Over/结束 Win/胜利
+    const gameStateRef = useRef('NotStart') //  同步游戏状态
     const [showOverlay, setShowOverlay] = useState(false);  // 全局蒙版
     const gameStateWord: any = {
         NotStart: "游戏未开始",
@@ -125,7 +126,7 @@ const Gobang = () => {
     // 落子
     const chessPieceDrop = (pointX: number, pointY: number, player?: string,) => {
         let copyData = [...pieces]
-        if (!copyData[pointX][pointY].color && (gameState == 'Start' || gameState == 'NotStart')) {
+        if (!copyData[pointX][pointY].color && (gameStateRef.current == 'Start' || gameStateRef.current == 'NotStart')) {
             copyData[pointX][pointY].color = rotationColor ? 'black' : 'white'
             setPieces(copyData)
             if (rotationColor) {
@@ -143,12 +144,12 @@ const Gobang = () => {
         setWhitePieces([])
         setPieces(generateData())
         setRotationColor(true)
-        setGameState('NotStart')
+        gameStateRef.current = 'NotStart'
     }
 
     // 切换开始方
     const changeStartColor = (color: string) => {
-        if (gameState == 'NotStart' && (rotationColor ? 'black' : 'white') != color) {
+        if (gameStateRef.current == 'NotStart' && (rotationColor ? 'black' : 'white') != color) {
             setRotationColor(pre => !pre)
         } else {
             // 做个全局提示
@@ -163,22 +164,21 @@ const Gobang = () => {
         // console.log('black', blackPieces)
         // console.log('white', whitePieces)
         if (blackPieces.length + whitePieces.length > 0) {
-            setGameState('Start')
+            gameStateRef.current = 'Start'
         }
         if (blackPieces.length >= 5) {
             if (isWin(blackPieces)) {
-                setGameState("Win")
                 setVictory(' 黑子 ')
             }
         }
         if (whitePieces.length >= 5) {
             if (isWin(whitePieces)) {
-                setGameState("Win")
                 setVictory(' 白子 ')
             }
         }
     }, [blackPieces, whitePieces])
 
+    // 判断是否存在胜利方
     const isWin = (data: Gobang[]) => {
         let gameResult: boolean = false
 
@@ -217,11 +217,16 @@ const Gobang = () => {
             }
         })
 
+        if (gameResult) {
+            gameStateRef.current = 'Win'
+        }
         return gameResult
     }
+
     useEffect(() => {
+        setGameState(gameStateRef.current)
         // console.log(gameState)
-        if (gameState == 'Win') {
+        if (gameStateRef.current == 'Win') {
             // console.log(blackPieces)
             // console.log(whitePieces)
             messageApi.open({
@@ -230,29 +235,205 @@ const Gobang = () => {
             })
             setShowOverlay(true)
         }
-    }, [gameState])
+    }, [gameStateRef.current])
+
+    useEffect(() => {
+        if (!rotationColor && gameStateRef.current == 'Start') {
+            setTimeout(() => {
+                computerChess()
+            }, 600)
+        }
+    }, [rotationColor])
 
     // 电脑落子
     const computerChess = () => {
         // 电脑落子 利用算法————权重值
+        // 初始棋盘全空值（未落子）的权重值为1，已落子的权重值为0，说明：权值只针对电脑使用，不计该点对玩家的权值
+        // 落子计算权值，以下是该空点若落子为哪方之后，计算权重值
+        // 连一，玩家+5，电脑+5
+        // 连二，玩家+10，电脑+10
+        // 死三，玩家+30，电脑+25
+        // 活三，玩家+40，电脑+50
+        // 死四，玩家+60，电脑+55
+        // 活四，玩家+200，电脑+300
+        // 连五，玩家+20000，电脑+30000
+
         // 判断哪一点的值最高，也就是对电脑的利益最大
         // 每下一步，就会判断某点对于玩家利益大还是自身利益大，来进行围堵和进攻
-        const playerScore: any = [];  	// 对于玩家而言，每一个空点的数值集合
-        const computerScore: any = [];	// 对于电脑而言，每一个空点的数值集合
+        const computerScore: any = [];	// 空点
         let maxScore = 0;			// 最大值
-        let x = 0, y = 0;			// 最后决定电脑落子的位置
+        let position: any = [] // 可以落子的位置
 
         // 初始化玩家和电脑每个点的数值
         for (let i = 0; i < 15; i++) {
-            playerScore[i] = [];
             computerScore[i] = [];
             for (let j = 0; j < 15; j++) {
-                playerScore[i][j] = 0;
                 computerScore[i][j] = 0;
             }
         }
-        console.log(pieces)
+        pieces.forEach((item, i) => {
+            item.forEach((items, j) => {
+                if (items.color != null) {
+                    computerScore[i][j] = 0
+                } else {
+                    computerScore[i][j] = weightValue("white", pieces, i, j)
+                }
+            })
+        })
+        console.log(computerScore)
+        // console.log(pieces)
 
+        maxScore = Math.max(...computerScore.flat(Infinity));  //  获取最大值
+
+        computerScore.forEach((item: number[], i: number) => {
+            item.forEach((items: number, j: number) => {
+                if (items == maxScore) {
+                    position.push([i, j])
+                }
+            })
+        })
+
+        if (position.length == 1) {
+            chessPieceDrop(position[0][0], position[0][1])
+        } else if (position.length > 0) {
+            let num = Rand(0, position.length - 1)
+            chessPieceDrop(position[num][0], position[num][1])
+        }
+
+    }
+    // 权重值
+    const weightValue = (color: string, data: any, x: number, y: number) => {
+        let max: number = 1
+        const contraryColor = color == 'black' ? 'white' : 'black'
+
+        let directs = [[1, 0], [1, 1], [0, 1], [-1,]];
+        for (let k = 0; k < 4; k++) {
+            let row = directs[k][0];
+            let col = directs[k][1];
+
+            //重置
+            let personNum = 0;  //  电脑连子数
+            let emptyNum = 0;   //  玩家围攻棋子数量
+            let botNum = 0; //  玩家连子数
+            let emptyNumT = 0; //  电脑围攻棋子数量
+
+            //! 该点对电脑权值
+            // 正向
+            for (let i = 1; i <= 4; i++) {
+                if (x + i * col >= 0 && x + i * col < 15 &&
+                    y + i * row >= 0 && y + i * row < 15 &&
+                    pieces[x + i * col]?.[y + i * row]?.color == color) {
+                    //玩家的子
+                    personNum++;
+                } else if (x + i * col >= 0 && x + i * col < 15 &&
+                    y + i * row >= 0 && y + i * row < 15 && pieces[x + i * col]?.[y + i * row]?.color == null) {
+                    //空白位
+                    emptyNum++;
+                    break;    //遇到空白位置停止该方向搜索
+                } else {
+                    break;    //出边界或遇到玩家棋子停止搜索
+                }
+            }
+            // 反向
+            for (let i = 1; i <= 4; i++) {
+                if (x - i * col >= 0 && x - i * col < 15 &&
+                    y - i * row >= 0 && y - i * row < 15 &&
+                    pieces[x - i * col]?.[y - i * row]?.color == color) {
+                    personNum++;
+                } else if (x - i * col >= 0 && x - i * col < 15 &&
+                    y - i * row >= 0 && y - i * row < 15 &&
+                    pieces[x - i * col]?.[y - i * row]?.color == null) {
+                    emptyNum++;
+                    break;
+                } else {
+                    break;
+                }
+            }
+
+            if (personNum == 1) {
+                max += 10;
+            } else if (personNum == 2) {
+                if (emptyNum == 1) {
+                    //死3
+                    max += 30;
+                } else if (emptyNum == 2) {
+                    //活3
+                    max += 40;
+                }
+            } else if (personNum == 3) {
+                if (emptyNum == 1) {
+                    //死4
+                    max += 60;
+                } else if (emptyNum == 2) {
+                    //活4
+                    max += 200;
+                }
+            } else if (personNum == 4) {
+                max += 20000;
+            }
+
+            //! 该点对玩家权值
+            // 正向
+            for (let i = 1; i <= 4; i++) {
+                if (x + i * col >= 0 && x + i * col < 15 &&
+                    y + i * row >= 0 && y + i * row < 15 &&
+                    pieces[x + i * col]?.[y + i * row]?.color == contraryColor) {
+                    //玩家的子
+                    botNum++;
+                } else if (x + i * col >= 0 && x + i * col < 15 &&
+                    y + i * row >= 0 && y + i * row < 15 && pieces[x + i * col]?.[y + i * row]?.color == null) {
+                    //空白位
+                    emptyNumT++;
+                    break;    //遇到空白位置停止该方向搜索
+                } else {
+                    break;    //出边界或遇到电脑棋子停止搜索
+                }
+            }
+            // 反向
+            for (let i = 1; i <= 4; i++) {
+                if (x - i * col >= 0 && x - i * col < 15 &&
+                    y - i * row >= 0 && y - i * row < 15 &&
+                    pieces[x - i * col]?.[y - i * row]?.color == contraryColor) {
+                    botNum++;
+                } else if (x - i * col >= 0 && x - i * col < 15 &&
+                    y - i * row >= 0 && y - i * row < 15 &&
+                    pieces[x - i * col]?.[y - i * row]?.color == null) {
+                    emptyNumT++;
+                    break;
+                } else {
+                    break;
+                }
+            }
+
+            if (botNum == 0) {
+                //连1
+                max += 5;
+            } else if (botNum == 1) {
+                //活2
+                max += 10;
+            } else if (botNum == 2) {
+                if (emptyNumT == 1) {
+                    //死3
+                    max += 25;
+                } else if (emptyNumT == 2) {
+                    //活3
+                    max += 50;
+                }
+            } else if (botNum == 3) {
+                if (emptyNumT == 1) {
+                    //死4
+                    max += 55;
+                } else if (emptyNumT == 2) {
+                    //活4
+                    max += 300;
+                }
+            } else if (botNum >= 4) {
+                //活5
+                max += 30000;
+            }
+        }
+
+        return max
     }
     // 随机数
     const Rand = (Min: number, Max: number) => {
