4ea9754b814827a4234d9bc4032e12c5cc971ee6
liuyuan <15713621441@163.com>
2024-01-15 17:14:35 +0800

    fix: watch组件赋值问题修复

--- src/components/Watch/index.tsx
+++ src/components/Watch/index.tsx
@@ -1,38 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './index.scss'
 
 export default function Watch() {
+    const [years, setYears] = useState<number>() // 年
+    const [months, setMonths] = useState<string>() // 月
+    const [days, setDays] = useState<string>() // 日
+    const [hours, setHours] = useState<string>() // 时
+    const [minutes, setMinutes] = useState<string>() // 分
+    const [seconds, setSeconds] = useState<string>() // 秒
 
     const showTime = () => {
         let date = new Date();
-        let year: any = date.getFullYear(); // 年
-        let month: any = ('0' + (date.getMonth() + 1)).slice(-2); // 月
-        let day: any = ('0' + date.getDate()).slice(-2); // 日
-        let hour: any = ('0' + date.getHours()).slice(-2); // 0 - 23  
-        let minute: any = ('0' + date.getMinutes()).slice(-2); // 0 - 59
-        let second: any = ('0' + date.getSeconds()).slice(-2); // 0 - 59
+        let year: number = date.getFullYear(); // 年
+        let month: string = ('0' + (date.getMonth() + 1)).slice(-2); // 月
+        let day: string = ('0' + date.getDate()).slice(-2); // 日
+        let hour: string = ('0' + date.getHours()).slice(-2); // 0 - 23  
+        let minute: string = ('0' + date.getMinutes()).slice(-2); // 0 - 59
+        let second: string = ('0' + date.getSeconds()).slice(-2); // 0 - 59
 
-        let years: any = document.getElementById("years")
-        let months: any = document.getElementById("months")
-        let days: any = document.getElementById("days")
-        let hours: any = document.getElementById("hours")
-        let minutes: any = document.getElementById("minutes")
-        let seconds: any = document.getElementById("seconds")
+        setYears(year)
+        setMonths(month)
+        setDays(day)
+        setHours(hour)
+        setMinutes(minute)
+        setSeconds(second)
 
-        years.innerText = year
-        years.textContent = year
-        months.innerText = month
-        months.textContent = month
-        days.innerText = day
-        days.textContent = day
-        hours.innerText = hour
-        hours.textContent = hour
-        minutes.innerText = minute
-        minutes.textContent = minute
-        seconds.innerText = second
-        seconds.textContent = second
-
-        setTimeout(showTime, 1000);
+        setTimeout(showTime, 200)
     }
 
     useEffect(() => {
@@ -40,14 +33,14 @@ export default function Watch() {
     }, [])
 
     return (
-        <div id="MyClockDisplay" className="clock">
+        <div className="clock">
             <div className='HMS'>
-                <div><span id="hours"></span>Hours</div>
-                <div><span id="minutes"></span>Minutes</div>
-                <div><span id="seconds"></span>Seconds</div>
+                <div><span>{hours}</span>Hours</div>
+                <div><span>{minutes}</span>Minutes</div>
+                <div><span>{seconds}</span>Seconds</div>
             </div>
             <div className='YMD'>
-                <span id="years"></span>-<span id="months"></span>-<span id="days"></span>
+                <span>{years}</span>-<span>{months}</span>-<span>{days}</span>
             </div>
         </div>
     )
