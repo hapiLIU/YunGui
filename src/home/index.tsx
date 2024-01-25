55b6acf25618abf12966ef56e8505d3c1f26ee19
liuyuan <15713621441@163.com>
2024-01-25 15:05:30 +0800

    feat: 图标卡片组件新增缎带展示

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -11,8 +11,6 @@ import tankBattle from '../image/坦克.jpg'
 import rottenPenHead from '../image/练习.jpg'
 import threejs from '../image/threejs.jpg'
 import chatRoom from '../image/聊天.jpg'
-import { useEffect, useState } from 'react'
-import { useHolidayAndSolarTerm } from '../services/hooks/perpetualCalendar'
 import FallingFlowers from '../components/FallingFlowers'
 
 export default function Home() {
@@ -24,14 +22,14 @@ export default function Home() {
                 <Watch />
             </div>
             <div className='content'>
-                <DesktopIcon icon={minesweeper} name='扫雷' route='/minesweeper'></DesktopIcon>
-                <DesktopIcon icon={sudoku} name='数独' route='/sudoku'></DesktopIcon>
-                <DesktopIcon icon={gobang} name='五子棋' route='/gobang'></DesktopIcon>
-                <DesktopIcon icon={playingCards} name='纸牌' route='/playingCards'></DesktopIcon>
-                <DesktopIcon icon={tankBattle} name='坦克大战' route='/tankBattle'></DesktopIcon>
-                <DesktopIcon icon={rottenPenHead} name='练习' route='/rottenPenHead'></DesktopIcon>
-                <DesktopIcon icon={threejs} name='ThreeJS' route='/threejs'></DesktopIcon>
-                <DesktopIcon icon={chatRoom} name='简易聊天室' route='/chatRoom'></DesktopIcon>
+                <DesktopIcon icon={minesweeper} name='扫雷' route='/minesweeper' ribbon={{ text: '测试阶段', color: 'green' }}></DesktopIcon>
+                <DesktopIcon icon={sudoku} name='数独' route='/sudoku' ribbon={{ text: '测试阶段', color: 'green' }}></DesktopIcon>
+                <DesktopIcon icon={gobang} name='五子棋' route='/gobang' ribbon={{ text: '测试阶段', color: 'green' }}></DesktopIcon>
+                <DesktopIcon icon={playingCards} name='纸牌' route='/playingCards' ribbon={{ text: '开发搁置', color: 'red' }}></DesktopIcon>
+                <DesktopIcon icon={tankBattle} name='坦克大战' route='/tankBattle' ribbon={{ text: '开发搁置', color: 'red' }}></DesktopIcon>
+                <DesktopIcon icon={rottenPenHead} name='练习' route='/rottenPenHead' ribbon={{ text: '个人练习', color: 'cyan' }}></DesktopIcon>
+                <DesktopIcon icon={threejs} name='ThreeJS' route='/threejs' ribbon={{ text: '个人练习', color: 'cyan' }}></DesktopIcon>
+                <DesktopIcon icon={chatRoom} name='简易聊天室' route='/chatRoom' ribbon={{ text: '开发搁置', color: 'red' }}></DesktopIcon>
             </div>
             <FloatMenu />
         </div>
