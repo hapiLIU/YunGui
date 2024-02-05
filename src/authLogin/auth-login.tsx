cde377fe1586dfbfa7e4d649ff0c43fda3f40166
liuyuan <15713621441@163.com>
2024-02-05 14:39:15 +0800

    feat: 登录注册页搭建

--- src/authLogin/auth-login.tsx
+++ src/authLogin/auth-login.tsx
@@ -1,77 +1,120 @@
 import { useEffect, useState } from 'react';
 import Cloud from '../components/Cloud'
 import './auth-login.scss'
-import { Button, Form, Image, Input } from 'antd';
+import { Button, DatePicker, Form, Image, Input } from 'antd';
 
 export default function AuthLogin() {
-    const onFinish = (values: any) => {
+
+    const [sign, setSign] = useState<"in" | "up">('in')
+
+    // 去登录
+    const goSignIn = (values: any) => {
         console.log('Success:', values);
     };
-
-    const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
+    // 去注册
+    const goSignUp = (values: any) => {
+        console.log('Success:', values);
     };
 
     return (
         <div className='login-menu'>
-            <div className='login-bg'>
-                <div className='cloud2' style={{ top: "5%", animationDelay: `${(Math.random() * 3) + "s"}`, animationDuration: `${(12 + Math.random() * 3) + "s"}` }}>
-                    <Cloud size='medium' direction='left' />
-                </div>
-                <div className='cloud1' style={{ top: "15%", animationDelay: `${(Math.random() * 3) + "s"}`, animationDuration: `${(10 + Math.random() * 3) + "s"}` }}>
-                    <Cloud size='small' />
-                </div>
-                <div className='cloud2' style={{ top: "25%", animationDelay: `${(Math.random() * 3) + "s"}`, animationDuration: `${(12 + Math.random() * 3) + "s"}` }}>
-                    <Cloud size='medium' direction='left' />
-                </div>
-                <div className='cloud1' style={{ top: "35%", animationDelay: `${(Math.random() * 3) + "s"}`, animationDuration: `${(10 + Math.random() * 3) + "s"}` }}>
-                    <Cloud size='small' />
-                </div>
-                <div className='cloud1' style={{ top: "40%", animationDelay: `${(Math.random() * 3) + "s"}`, animationDuration: `${(15 + Math.random() * 3) + "s"}` }}>
-                    <Cloud size='large' />
-                </div>
-                <div className='cloud1' style={{ top: "65%", animationDelay: `${(Math.random() * 3) + "s"}`, animationDuration: `${(12 + Math.random() * 3) + "s"}` }}>
-                    <Cloud size='medium' />
-                </div>
-                <div className='cloud2' style={{ top: "75%", animationDelay: `${(Math.random() * 3) + "s"}`, animationDuration: `${(10 + Math.random() * 3) + "s"}` }}>
-                    <Cloud size='small' direction='left' />
-                </div>
-                <div className='cloud1' style={{ top: "88%", animationDelay: `${(Math.random() * 3) + "s"}`, animationDuration: `${(12 + Math.random() * 3) + "s"}` }}>
-                    <Cloud size='medium' />
-                </div>
-            </div>
             <div className='login-form'>
                 <Image className='icon' src={require("../image/logo/logo1.png")} preview={false} />
-                <div className='form-content'>
-                    <h2>登录</h2>
-                    <Form
-                        name="basic"
-                        style={{ maxWidth: 400 }}
-                        initialValues={{ remember: true }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                    >
-                        <Form.Item
-                            name="username"
-                            rules={[{ required: true, message: '请输入账号!' }]}
+                {sign == 'in' ? (
+                    <div className='form-content'>
+                        <h2>登录</h2>
+                        <Form
+                            name="basic"
+                            style={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+                            initialValues={{ remember: true }}
+                            onFinish={goSignIn}
                         >
-                            <Input placeholder="账号" />
-                        </Form.Item>
+                            <Form.Item
+                                name="account"
+                                rules={[{ required: true, message: '请输入账号!' }]}
+                                style={{ width: "60%" }}
+                            >
+                                <Input placeholder="账号" />
+                            </Form.Item>
 
-                        <Form.Item
-                            name="password"
-                            rules={[{ required: true, message: '请输入密码!' }]}
+                            <Form.Item
+                                name="password"
+                                rules={[{ required: true, message: '请输入密码!' }]}
+                                style={{ width: "60%" }}
+                            >
+                                <Input.Password placeholder="密码" />
+                            </Form.Item>
+
+                            <Form.Item style={{ width: "60%" }}>
+                                <Button type="primary" htmlType="submit" style={{ width: "100%", backgroundColor: "#009fdc" }}>
+                                    登录
+                                </Button>
+                            </Form.Item>
+
+                            <div className='bottomIn'>
+                                <div onClick={() => setSign("up")}>没有账号？前往注册</div>
+                                <div>忘记密码？</div>
+                            </div>
+                        </Form>
+                    </div>
+                ) : (
+                    <div className='form-content'>
+                        <h2>注册</h2>
+                        <Form
+                            name="basic"
+                            style={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+                            initialValues={{ remember: true }}
+                            onFinish={goSignUp}
                         >
-                            <Input.Password placeholder="密码" />
-                        </Form.Item>
+                            <Form.Item
+                                name="account"
+                                rules={[{ required: true, message: '请输入账号!' }]}
+                                style={{ width: "60%" }}
+                            >
+                                <Input placeholder="账号" />
+                            </Form.Item>
 
-                        <Form.Item>
-                            <Button type="primary" htmlType="submit" style={{ width: "100%", backgroundColor: "#009fdc" }}>
-                                登录
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
+                            <Form.Item
+                                name="password"
+                                rules={[{ required: true, message: '请输入密码!' }]}
+                                style={{ width: "60%" }}
+                            >
+                                <Input.Password placeholder="密码" />
+                            </Form.Item>
+
+                            <Form.Item
+                                name="name"
+                                style={{ width: "60%" }}
+                            >
+                                <Input placeholder="昵称（选填）" />
+                            </Form.Item>
+
+                            <Form.Item
+                                name="email"
+                                style={{ width: "60%" }}
+                            >
+                                <Input placeholder="邮箱（选填）" />
+                            </Form.Item>
+
+                            <Form.Item
+                                name="birthday"
+                                style={{ width: "60%" }}
+                            >
+                                <DatePicker placeholder="生日（选填）" style={{ width: "100%" }} />
+                            </Form.Item>
+
+                            <Form.Item style={{ width: "60%" }}>
+                                <Button type="primary" htmlType="submit" style={{ width: "100%", backgroundColor: "#009fdc" }}>
+                                    注册
+                                </Button>
+                            </Form.Item>
+
+                            <div className='bottomUp'>
+                                <div onClick={() => setSign("in")}>返回登录</div>
+                            </div>
+                        </Form>
+                    </div>
+                )}
             </div>
         </div>
     )
